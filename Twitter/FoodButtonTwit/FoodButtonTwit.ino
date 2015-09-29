1386347aeba0ca19939b5f385833f2afc1650639
Dominic Grande <dominic.c.grande@gmail.com>
2015-09-28 23:06:29 -0500

    Using new code from Arduino website

---
+++ Twitter/FoodButtonTwit/FoodButtonTwit.ino
@@ -0,0 +1,35 @@
+/* Post a simple message to Twitter  */
+#include <SPI.h>
+#include <Ethernet.h>
+#include <Twitter.h>
+
+byte mac[] = { 0x90, 0xA2, 0xDA, 0x0E, 0x92, 0xA4 };
+byte ip[] = { 192, 168, 100, 9 };
+
+Twitter twitter("Replcae this text with your token");
+char msg[] = "Hello from Arduino & Ethernet Shield!";
+
+void setup()
+{
+  Ethernet.begin(mac, ip);
+  Serial.begin(9600);
+
+  delay(1000);
+
+  Serial.println("connecting ...");
+  if (twitter.post(msg)) {
+    int status = twitter.wait(&Serial);
+    if (status == 200) {
+      Serial.println("OK.");
+    } else {
+      Serial.print("failed : code ");
+      Serial.println(status);
+    }
+  } else {
+    Serial.println("connection failed.");
+  }
+}
+
+void loop()
+{
+}
